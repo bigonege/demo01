ac591822ec8253ed1799549c6940a015df205e05
xpcloud <15170942311@163.com>
2019-07-17 19:43:27 +0800

    test1.0

---
+++ pages/index/index.js
@@ -0,0 +1,31 @@
+//index.js
+//获取应用实例
+Page({
+  data: {
+    focus: false,
+    inputValue: '',
+    imgUrls:[
+      'http://img2.imgtn.bdimg.com/it/u=1946009481,1756887209&fm=26&gp=0.jpg',
+      'https://gd2.alicdn.com/imgextra/i2/3477075384/O1CN01rImJ1K1pduxfnmVtR_!!3477075384.jpg',
+'https://img.alicdn.com/imgextra/i3/4207517744/O1CN01XvqXcR274ncCzFWaq_!!0-item_pic.jpg_430x430q90.jpg'
+
+    ],
+    indicatorDots: true,
+    autoplay: true,
+    interval: 3000,
+    duration: 1000,
+  },
+
+  durationChange:function(e)
+  {
+    this.setData({
+      duration: e.detail.value
+    })
+  },
+
+  bindButtonTap: function () {
+    this.setData({
+      focus: true
+    })
+  },
+})
